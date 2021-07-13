fe3deaf666d90f322c248f78e03d5594c83b83b2
김지은 <kimmjieun@pincar.co.kr>
2021-07-13 23:17:39 +0900

    20. useReducer study

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import React, { useState,useRef, useMemo,useCallback } from 'react';
+import React, { useState,useRef, useMemo,useCallback,useReducer } from 'react';
 import './App.css';
 
 import CreateUser from './CreateUser';
@@ -9,22 +9,13 @@ function countActiveUsers(users) {
   console.log('활성 사용자 수를 세는중...');
   return users.filter(user => user.active).length;
 }
-function App() {
-  const [inputs, setInputs] = useState({
+
+const initialState = {
+  inputs: {
     username: '',
     email: ''
-  });
-  const { username, email } = inputs;
-  const onChange = e => { //ㅁ모르겟
-    const { name, value } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value
-    });
-  };
-
-
-  const [users, setUsers] = useState([
+  },
+  users: [
     {
       id: 1,
       username: 'velopert',
@@ -43,56 +34,167 @@ function App() {
       email: 'liz@example.com',
       active: false
     }
-  ]);
-  const nextId = useRef(4);
-  const onCreate = useCallback(() => {
-    const user = {
-      id: nextId.current,
-      username,
-      email
-    };
-    setUsers(users.concat(user));
+  ]
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CHANGE_INPUT':
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.name]: action.value
+        }
+      };
+    case 'CREATE_USER':
+      return {
+        inputs: initialState.inputs,
+        users: state.users.concat(action.user)
+      };
+    case 'REMOVE_USER':
+      return {
+        ...state,
+        users: state.users.filter(user => user.id !== action.id)
+      };
+    default:
+      return state;
+  }
+}
 
-    setInputs({
-      username: '',
-      email: ''
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const nextId = useRef(4);
+  const { users } = state;
+  const { username, email } = state.inputs;
+
+  const onChange = useCallback(e => {
+    const { name, value } = e.target;
+    dispatch({
+      type: 'CHANGE_INPUT',
+      name,
+      value
+    });
+  }, []);
+
+  const onCreate = useCallback(() => {
+    dispatch({
+      type: 'CREATE_USER',
+      user: {
+        id: nextId.current,
+        username,
+        email
+      }
     });
     nextId.current += 1;
-  }, [users, username, email]);
+  }, [username, email]);
+  const onToggle = useCallback(id => {
+    dispatch({
+      type: 'TOGGLE_USER',
+      id
+    });
+  }, []);
 
-  const onRemove = useCallback(
-    id => {
-      // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
-      // = user.id 가 id 인 것을 제거함
-      setUsers(users.filter(user => user.id !== id));
-    },
-    [users]
-  );
-  const onToggle = useCallback(
-    id => {
-      setUsers(
-        users.map(user =>
-          user.id === id ? { ...user, active: !user.active } : user
-        )
-      );
-    },
-    [users]
-  );
-  const count = useMemo(() => countActiveUsers(users), [users]);
-
+  const onRemove = useCallback(id => {
+    dispatch({
+      type: 'REMOVE_USER',
+      id
+    });
+  }, []);
   return (
     <>
       <CreateUser
         username={username}
         email={email}
         onChange={onChange}
-        onCreate={onCreate}
-      />
-      {/*<UserList users={users} onRemove={onRemove} onToggle={onToggle}/>*/}
-      {/*<div>활성사용자 수 : {count}</div>*/}
-      {/*test*/}
+        onCreate={onCreate}/>
+      <UserList users={users} onToggle={onToggle} onRemove={onRemove}/>
+      <div>활성사용자 수 : 0</div>
     </>
   );
+  // const [inputs, setInputs] = useState({
+  //   username: '',
+  //   email: ''
+  // });
+  // const { username, email } = inputs;
+  // const onChange = e => { //ㅁ모르겟
+  //   const { name, value } = e.target;
+  //   setInputs({
+  //     ...inputs,
+  //     [name]: value
+  //   });
+  // };
+  //
+  //
+  // const [users, setUsers] = useState([
+  //   {
+  //     id: 1,
+  //     username: 'velopert',
+  //     email: 'public.velopert@gmail.com',
+  //     active: true
+  //   },
+  //   {
+  //     id: 2,
+  //     username: 'tester',
+  //     email: 'tester@example.com',
+  //     active: false
+  //   },
+  //   {
+  //     id: 3,
+  //     username: 'liz',
+  //     email: 'liz@example.com',
+  //     active: false
+  //   }
+  // ]);
+  // const nextId = useRef(4);
+  // const onCreate = useCallback(() => {
+  //   const user = {
+  //     id: nextId.current,
+  //     username,
+  //     email
+  //   };
+  //   setUsers(users.concat(user));
+  //
+  //   setInputs({
+  //     username: '',
+  //     email: ''
+  //   });
+  //   nextId.current += 1;
+  // }, [users, username, email]);
+  //
+  // const onRemove = useCallback(
+  //   id => {
+  //     // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+  //     // = user.id 가 id 인 것을 제거함
+  //     setUsers(users.filter(user => user.id !== id));
+  //   },
+  //   [users]
+  // );
+  // const onToggle = useCallback(
+  //   id => {
+  //     setUsers(
+  //       users.map(user =>
+  //         user.id === id ? { ...user, active: !user.active } : user
+  //       )
+  //     );
+  //   },
+  //   [users]
+  // );
+  // const count = useMemo(() => countActiveUsers(users), [users]);
+  //
+  //
+  // return (
+  //   <>
+  //
+  //     <CreateUser
+  //       username={username}
+  //       email={email}
+  //       onChange={onChange}
+  //       onCreate={onCreate}
+  //     />
+  //     <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+  //     <div>활성사용자 수 : {count}</div>
+  //   </>
+  // );
 
 }
 
